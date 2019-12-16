6a862040b3ba17fb5cc45991cde63bc4cadaef5e
Carson McManus <carson.mcmanus1@gmail.com>
2019-12-16 10:24:03 -0500

    set up webpack and babel

---
+++ sec.club/webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+			{
+				test: /\.(s?)[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|svg|jp(|e)g|gif)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.(woff(|2)|eot|ttf|otf)$/,
+				use: ['file-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new CleanWebpackPlugin(),
+	],
+	devServer: {
+		contentBase: './public',
+		hot: true,
+	}
+};
