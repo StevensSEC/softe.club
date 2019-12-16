0538eadd188ff7e833f8cda97fb746b12553f9e6
Bruno Salgado <bsalgado@stevens.edu>
2019-12-15 19:26:13 -0500

    Removed reactstrap and added material ui instead. Header reflects material ui usage

--- sec.club/src/App.js
+++ sec.club/src/App.js
@@ -2,12 +2,24 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.scss"
 import Header from "./components/Header/Header"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core"
 
 function App() {
+  const theme = createMuiTheme({
+    text: {
+      //change these to values in variables.scss when webpack is set up
+      primary: "black",
+      secondary: "red",
+      tertiary: "cyan",
+      quaternary: "white"
+    }
+  })
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header></Header>
+      </div>
+    </ThemeProvider>
   )
 }
 
--- sec.club/src/components/Header/Header.jsx
+++ sec.club/src/components/Header/Header.jsx
@@ -1,23 +1,77 @@
-import React, { useState } from "react"
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import {
+  Drawer,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Typography
+} from "@material-ui/core"
 import "./Header.scss"
-const secLogo = require("../../assets/sec-logo.png")
-const Header = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => {
-    setIsOpen(prev => !prev)
+
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
   }
+})
+
+export default function TemporaryDrawer() {
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  })
+
+  const toggleDrawer = (side, open) => event => {
+    //Extra key navigation functionality
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
+
+    setState({ ...state, [side]: open })
+  }
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Semester Long Project", "What is SEC?", "The Team", "Contact"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+    </div>
+  )
+
   return (
     <nav id="header">
       <div id="header-brand">
-        <h1>Software Engineering Club</h1>
+        <Typography variant="h4">Software Engineering Club</Typography>
       </div>
       <div id="header-items">
-        <a className="HeaderItem">Semester Long Project</a>
-        <a className="HeaderItem">What is SEC?</a>
-        <a className="HeaderItem">Our Team</a>
-        <a className="HeaderItem">Contact</a>
+        <Button onClick={toggleDrawer("left", true)} style={{ color: "white" }}>
+          Menu
+        </Button>
       </div>
+      <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+        {sideList("left")}
+      </Drawer>
     </nav>
   )
 }
-export default Header
