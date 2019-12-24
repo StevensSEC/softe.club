9afdbe936dfea1f985747d295f45ee1ec2a7c646
Carson McManus <carson.mcmanus1@gmail.com>
2019-12-24 17:23:33 -0500

    move all pages to markdown files

--- sec.club/src/App.js
+++ sec.club/src/App.js
@@ -3,7 +3,6 @@ import "./App.scss"
 import Header from "./components/Header/Header"
 import { createMuiTheme, ThemeProvider } from "@material-ui/core"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import ResourcesView from "./views/Resources/Resources.js";
 import ArticleView from "./views/Article/Article.js";
 
 function App() {
@@ -23,19 +22,19 @@ function App() {
           <Header></Header>
           <Switch>
             <Route path="/slp">
-              <h1>Add SLP info page here</h1>
+              <ArticleView source="slp.md" />
             </Route>
             <Route path="/about">
               <ArticleView source="about.md" />
             </Route>
             <Route path="/team">
-              <h1>Add team information page here</h1>
+              <ArticleView source="team.md" />
             </Route>
             <Route path="/contact">
-              <h1>Add contact page here</h1>
+              <ArticleView source="contact.md" />
             </Route>
             <Route path="/resources">
-              <ResourcesView />
+              <ArticleView source="resources.md" />
             </Route>
             <Route exact path="/">
               <h1>hi this is home :)</h1>
