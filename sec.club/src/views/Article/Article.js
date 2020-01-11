bdbdcec069458fcb053ef9f809a97a1877f17f7e
Carson McManus <carson.mcmanus1@gmail.com>
2020-01-11 17:53:26 -0500

    make links in markdown articles readable

--- sec.club/src/views/Article/Article.js
+++ sec.club/src/views/Article/Article.js
@@ -2,6 +2,7 @@ import React from "react";
 import Markdown from "react-markdown";
 import HtmlParser from "react-markdown/plugins/html-parser";
 import { Container } from "@material-ui/core";
+import "./Article.scss";
 
 // See https://github.com/aknuds1/html-to-react#with-custom-processing-instructions
 // for more info on the processing instructions
