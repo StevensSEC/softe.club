64a53f7760519b376e56bf386890fc42e5590f1a
Carson McManus <carson.mcmanus1@gmail.com>
2020-01-11 19:05:54 -0500

    add hero element to home page

--- sec.club/src/views/Home/Home.js
+++ sec.club/src/views/Home/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Box, Container, Grid } from "@material-ui/core";
 import "./Home.scss";
 
 export default function HomeView() {
 	return (
-		<Container>
-			<h1>hi this is home :)</h1>
-			<p>this is a paragraph</p>
-		</Container>
+		<Box>
+			<Grid container direction="column" justify="center" alignItems="center" className="hero">
+				<Grid item>
+					<h1>SEC</h1>
+				</Grid>
+				<Grid item>
+					<span>Software Engineering Club</span>
+				</Grid>
+			</Grid>
+			<Container>
+				<h1>hi this is home :)</h1>
+				<p>this is a paragraph</p>
+			</Container>
+		</Box>
 	)
 }
