eeb025bff181405064b42c44aff5ad5ac1039122
Carson McManus <carson.mcmanus1@gmail.com>
2020-01-22 14:23:03 -0500

    build fonts and images with hashes to avoid cache errors

--- sec.club/webpack.config.js
+++ sec.club/webpack.config.js
@@ -18,11 +18,21 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jp(|e)g|gif)$/,
-        use: ["file-loader"]
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "img/[name].[hash:6].[ext]"
+          }
+        }]
       },
       {
         test: /\.(woff(|2)|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[hash:6].[ext]"
+          }
+        }]
       },
       {
         test: /\.(md)$/,
