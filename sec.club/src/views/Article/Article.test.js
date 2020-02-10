db57e7eeefd7373451842f3373f8b231a359820b
Carson McManus <carson.mcmanus1@gmail.com>
2020-02-10 18:03:50 -0500

    use jest-fetch-mock, fix unreasonable brittleness of article tests

--- sec.club/src/views/Article/Article.test.js
+++ sec.club/src/views/Article/Article.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
-import "@testing-library/dom";
+import { getByTestId, queryByTestId } from "@testing-library/dom";
 import "@testing-library/jest-dom";
 
 import ArticleView from './Article.js';
@@ -9,6 +9,7 @@ import ArticleView from './Article.js';
 let container = null;
 describe('ArticleView', () => {
 	beforeEach(() => {
+		fetch.resetMocks();
 		container = document.createElement('div');
 	});
 
@@ -27,19 +28,12 @@ describe('ArticleView', () => {
 This is a test markdown file used in unit tests.
 		`;
 
-		jest.spyOn(global, "fetch").mockImplementation(() =>
-			Promise.resolve({
-				text: () => Promise.resolve(markdown),
-			})
-		);
+		fetch.mockResponse(() => Promise.resolve(markdown));
 
 		await act(async () => {
 			render(<ArticleView source="test/placeholder.md" title="Basic" />, container);
 		});
-		expect(global.fetch).toHaveBeenCalled();
 		expect(container).toContainHTML('<h1>Basic</h1>');
-
-		global.fetch.mockRestore();
 	});
 
 	it('should not render `script` tags in markdown files', async () => {
@@ -51,21 +45,19 @@ This is a test markdown file used in unit tests.
 Script tags are not allowed in markdown files. This is done on purpose to
 prevent *you* from doing stupid stuff. If you need custom functionality on
 a page, you need to make your own view in \`src/views/\`.
+
+<script data-testid="test-script">
+console.error("YOU SHOULD NEVER SEE THIS TEXT");
+</script>
 		`;
 
-		jest.spyOn(global, "fetch").mockImplementation(() =>
-			Promise.resolve({
-				text: () => Promise.resolve(markdown),
-			})
-		);
+		fetch.mockResponse(() => Promise.resolve(markdown));
 
 		await act(async () => {
 			render(<ArticleView source="test/placeholder.md" title="No scripts allowed" />, container);
 		});
-		expect(container.innerHTML).toContain('<h1>No Scripts Allowed</h1>');
-		expect(container.innerHTML).not.toContain('<script>');
-
-		global.fetch.mockRestore();
+		expect(container).toContainHTML('<h1>No Scripts Allowed</h1>');
+		expect(queryByTestId(container, "test-script")).toBeNull();
 	});
 
 	it('should render `iframe` tags in markdown files', async () => {
@@ -77,21 +69,17 @@ This is a test markdown file used in unit tests.
 Iframes are allowed in markdown files. They can be used to embed
 youtube videos or codepens.
 
-<iframe data-test-id="test-iframe" width="560" height="315" src="https://www.youtube.com/embed/fBw7a0jRWOE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe data-testid="test-iframe" width="560" height="315" src="https://www.youtube.com/embed/fBw7a0jRWOE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		`;
 
-		jest.spyOn(global, "fetch").mockImplementation(() =>
-			Promise.resolve({
-				text: () => Promise.resolve(markdown),
-			})
-		);
+		fetch.mockResponse(() => Promise.resolve(markdown));
 
 		await act(async () => {
 			render(<ArticleView source="README.md" title="Iframes allowed" />, container);
 		});
 		expect(container).toContainHTML('<h1>Iframes Allowed</h1>');
-		expect(container).toContainElement(getByTestId('test-iframe'));
-
-		global.fetch.mockRestore();
+		let iframe = getByTestId(container, 'test-iframe');
+		expect(iframe).toBeDefined();
+		expect(container).toContainElement(iframe);
 	});
 })
