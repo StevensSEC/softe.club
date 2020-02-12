12a79512df97f85d4449bca7baf3497955fa445a
Bruno Salgado <bsalgado@stevens.edu>
2020-02-12 15:20:41 -0500

    Scrolling and article rendering functionality

--- sec.club/src/components/ProjectWall/ProjectWall.js
+++ sec.club/src/components/ProjectWall/ProjectWall.js
@@ -1,86 +1,99 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./ProjectWall.scss"
-import { GridList, GridListTile, GridListTileBar } from "@material-ui/core"
+import {
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  Paper
+} from "@material-ui/core"
 import Project from "./Project/Project"
 import "../../assets/logo.scss"
 import secLogo from "../../assets/logo.svg"
-import openTogetherTubeLogo from "../../assets/project-wall/open-together-tube-logo.svg"
+import nextIcon from "../../assets/next.svg"
+import PROJECTS from "../../views/SLP/Projects"
 
 const styles = {
   projectWall: {
+    position: "relative",
     flexWrap: "nowrap",
     height: "10%",
-    overflow: "hidden"
+    overflow: "hidden",
+    scrollBehavior: "smooth"
   },
   project: {
     width: "20%",
     display: "flex",
     flexDirection: "column"
+  },
+  // scrollButton: {
+  //   backgroundColor: "white",
+  //   position: "absolute",
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   borderRadius: "50%",
+  //   zIndex: 2,
+  //   width: "25px",
+  //   height: "auto",
+  //   right: "2%",
+  //   top: "50%"
+  // }
+  scrollContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%"
+  },
+  scrollButton: {
+    backgroundColor: "white",
+    width: "50%",
+    display: "flex",
+    justifyContent: "center",
+    opacity: 0.5
   }
 }
 
-const ProjectWall = () => {
-  const PROJECTS = [
-    {
-      img: null,
-      caption: "Quick Quack",
-      semester: "Spring",
-      year: "2019"
-    },
-    {
-      img: openTogetherTubeLogo,
-      caption: "Open Together Tube",
-      semester: "Fall",
-      year: "2019"
-    },
-    {
-      img: null,
-      caption: "Coming very soon! lorem iupsu mfdskaj klfdkls afj dklsaj f",
-      semester: "Spring",
-      year: "2020"
-    },
-    {
-      img: null,
-      caption: "Coming soon!",
-      semester: "Fall",
-      year: "2020"
-    },
-    {
-      img: null,
-      caption: "Coming soon!",
-      semester: "Spring",
-      year: "2021"
-    },
-    {
-      img: null,
-      caption: "Coming soon!",
-      semester: "Fall",
-      year: "2021"
-    },
-    {
-      img: null,
-      caption: "Coming soon!",
-      semester: "Spring",
-      year: "2022"
-    },
-    {
-      img: null,
-      caption: "Coming soon!",
-      semester: "Fall",
-      year: "2022"
-    }
-  ]
+const ProjectWall = ({ setProject }) => {
+  const scroll = elementIndex => {
+    // Rudimentary functionality for now: scroll to end or scoll to beginning
+    // TODO: Scroll to first element not visible in current viewport
+    document.getElementById(`project-${elementIndex}`).scrollIntoView()
+  }
   return (
-    <GridList style={styles.projectWall}>
-      {PROJECTS.map((project, index) => {
-        return (
-          <GridListTile style={styles.project}>
-            <img src={project.img} />
-            <GridListTileBar title={project.caption}></GridListTileBar>
-          </GridListTile>
-        )
-      })}
-    </GridList>
+    <>
+      <GridList style={styles.projectWall} id="project-wall">
+        {PROJECTS.map((project, index) => {
+          return (
+            <GridListTile
+              style={styles.project}
+              id={`project-${index}`}
+              onClick={() => setProject(project.caption)}
+            >
+              <img src={project.img} />
+              <GridListTileBar title={project.caption}></GridListTileBar>
+            </GridListTile>
+          )
+        })}
+      </GridList>
+      <div style={styles.scrollContainer}>
+        <div
+          style={{ ...styles.scrollButton, borderRight: "2px solid black" }}
+          onClick={() => scroll(0)}
+        >
+          <img
+            src={nextIcon}
+            style={{
+              display: "inline-block",
+              transform: "rotate(180deg)"
+            }}
+          ></img>
+        </div>
+        <div
+          style={{ ...styles.scrollButton, borderLeft: "2px solid black" }}
+          onClick={() => scroll(PROJECTS.length - 1)}
+        >
+          <img src={nextIcon}></img>
+        </div>
+      </div>
+    </>
   )
 }
 export default ProjectWall
--- sec.club/src/views/SLP/SLP.js
+++ sec.club/src/views/SLP/SLP.js
@@ -1,17 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import { Box, Container, Grid, Paper } from "@material-ui/core"
 import ProjectWall from "../../components/ProjectWall/ProjectWall"
+import PROJECTS from "./Projects"
 
 const styles = {
   projectWall: {
     height: "100%"
+  },
+  content: {
+    padding: "5%"
   }
 }
 
 const SLPView = () => {
+  const [project, setProject] = useState(PROJECTS[0])
+  console.log(project)
   return (
     <div style={styles.projectWall}>
-      <ProjectWall />
+      <ProjectWall setProject={setProject} />
+      <div style={styles.content}>
+        <h1>{project.caption}</h1>
+        <h3>{project.semester + " " + project.year}</h3>
+      </div>
     </div>
   )
 }
