2fd7c5674df21cdeac8a42b43dc8dce357006781
Carson McManus <carson.mcmanus1@gmail.com>
2020-02-14 17:59:45 -0500

    fix setupTests imports

--- sec.club/src/setupTests.js
+++ sec.club/src/setupTests.js
@@ -1,6 +1,4 @@
-require('jest-fetch-mock').enableMocks()
-
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 configure({ adapter: new Adapter() });
+require('jest-fetch-mock').enableMocks()
