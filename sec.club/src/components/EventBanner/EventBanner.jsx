683ac40f188dcc46c1e6097cc11f21d45a5b2b17
David Carpenter <carpenterd777@gmail.com>
2020-02-14 11:00:45 -0500

    Hides banner when end date exceeded

--- sec.club/src/components/EventBanner/EventBanner.jsx
+++ sec.club/src/components/EventBanner/EventBanner.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./EventBanner.scss";
 
-export default function EventBanner({flyerSource, title, desc}) {
+export default function EventBanner({flyerSource, title, desc, endDate}) {
     let flyer = require(`../../assets/flyers/${flyerSource}`)
 
-    return(
-        <div className="flyer">
-            <div className="container">
-                <img src={flyer.default} alt={`${flyerSource}`}/>
-                <div className="text-container">
-                    <span className="title">{title}</span>
-                    <span className="description">{desc}</span>
+    if (new Date().getTime() < endDate.getTime() || endDate === undefined) {
+        return(
+            <div className="flyer">
+                <div className="container">
+                    <img src={flyer.default} alt={`${flyerSource}`}/>
+                    <div className="text-container">
+                        <span className="title">{title}</span>
+                        <span className="description">{desc}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    } else {
+        return <div></div>
+    }
+}   
