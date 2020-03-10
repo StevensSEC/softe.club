9891f2aff97f24252cef0e7cb461043b443a16cf
David Carpenter <carpenterd777@gmail.com>
2020-03-10 14:13:22 -0400

    Project wall extends and displays names while extended

--- sec.club/src/components/ProjectWall/ProjectWall.js
+++ sec.club/src/components/ProjectWall/ProjectWall.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from 'prop-types'
 import "./ProjectWall.scss"
 import { NavLink } from "react-router-dom"
 import { motion, useViewportScroll } from 'framer-motion'
@@ -8,24 +9,44 @@ import CloseIcon from '@material-ui/icons/Close';
 import Button from "../Button/Button.js";
 import PROJECTS from "../../views/SLP/Projects"
 
-const Project = ({ project, className, onClick }) => (
-  <NavLink kind="menu" to={`/slp/${project.path}`} className={`project${className ? " " + className : ""}`} activeClassName="active" onClick={onClick}>
+class Project extends React.Component {
+  static propTypes = {
+		project: PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super();
+    this.props = props;
+    this.state = {showName: this.props.showName}
+  }
+
+  render() {
+    return (
+      <NavLink 
+    kind="menu" 
+    to={`/slp/${this.props.project.path}`} 
+    className={`project${this.props.className ? " " + this.props.className : ""}`} 
+    activeClassName="active" 
+  >
     <div className="img-container">
-      <img src={project.img} alt={`Logo for ${project.name}`} />
+      <img src={this.props.project.img} alt={`Logo for ${this.props.project.name}`} />
     </div>
-    {/* <div className="name">
-      <span>{project.name}</span>
-      <span>{`${project.semester} ${project.year}`}</span>
-    </div> */}
+    {this.props.showName && (<div className="name">
+      <span>{this.props.project.name}</span>
+      <span>{`${this.props.project.semester} ${this.props.project.year}`}</span>
+    </div>)}
   </NavLink>
-)
+    )
+  }
+}
 
 const ProjectWall = () => {
   const [state, setState] = React.useState({
     top: false,
     left: false,
     bottom: false,
-    right: false
+    right: false,
+    showNames: false,
   })
 
   const toggleDrawer = (side, open) => event => {
@@ -40,12 +61,32 @@ const ProjectWall = () => {
     setState({ ...state, [side]: open })
   }
 
+  const handleOnMouseEnter = () => {
+    if (state.showNames) { //only set the state if it needs to be reset
+      return;
+    } else {
+      setState({...state, showNames: true});
+    }
+  }
+
+  const handleOnMouseLeave = () => {
+    if (!state.showNames) {
+      return;
+    } else {
+      setState({...state, showNames: false});
+    }
+  }
+
   const { scrollY } = useViewportScroll();
 
   return (
-    <motion.div className="project-wall" y={scrollY}>
+    <motion.div 
+      className="project-wall" 
+      y={scrollY}
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}>
       {PROJECTS.map((project, index) => (
-        <Project project={project} key={`project-${index}`} />
+        <Project project={project} key={`project-${index}`} showName={state.showNames}/>
       ))}
       {/* <Button kind="icon" className="mobile" onClick={toggleDrawer("top", true)}>
         <MenuIcon />
