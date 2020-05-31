176db69d9d37a97b9dc502a52c3b303a7edb71c8
David Carpenter <carpenterd777@gmail.com>
2020-05-31 17:51:05 -0400

    Removed description, names don't wrap lines, improved mobile

--- sec.club/src/views/Team/Team.jsx
+++ sec.club/src/views/Team/Team.jsx
@@ -9,42 +9,36 @@ export default class TeamView extends Component{
         {
             role: "President",
             name: "Carson McManus",
-            desc: "In charge of the club's direction; guides each member to realize their potential in software engineering.",
             img: "carson.png",
             github: "https://github.com/dyc3",
         },
         {
             role: "Vice President",
             name: "Sarah Wiessler",
-            desc: "Provides additional direction to the club; manages the tasks of each member on the e-board.",
             img: "sarah.png",
             github: "https://github.com/SWiessler",
         },
         {
             role: "Treasurer",
             name: "David Carpenter",
-            desc: "Responsible for club finance, budgets and makes purchases on behalf of the club.",
             img: "david.jpeg",
             github: "https://github.com/carpenterd777",
         },
         {
             role: "Secretary",
             name: "Adrian Garcia",
-            desc: "Disseminates information; responsible for notifying members of events and opportunities!",
             img: "adrian.jpeg",
             github: "https://github.com/adrian-d-garcia",
         },
         {
             role: "Logistics Chair",
             name: "Abdullah Hyder",
-            desc: "Event organizer; ensures club events have a venue.",
             img: "user.jpg",
             github: "https://github.com/abdullahhyder",
         },
         {
             role: "Marketing Chair",
             name: "Anjali Paliwal",
-            desc: "Shows off the fun we have at each event by sharing photos on Instagram and GroupMe!",
             img: "user.jpg",
             github: "https://github.com/grinchpal",
         }
@@ -75,7 +69,6 @@ export default class TeamView extends Component{
                                 <img src={member.img} alt={member.name}/>
                                 <h2>{member.name}</h2>
                                 <div className="role">{member.role}</div>
-                                <div className="desc">{member.desc}</div>
                             </Grid>
                         </a>
                         )
