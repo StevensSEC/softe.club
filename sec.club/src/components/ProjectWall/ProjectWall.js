b20031028ee5ca6feafb4f809835f0e6c7e850c2
David Carpenter <carpenterd777@gmail.com>
2020-06-01 17:44:00 -0400

    Cut away extraneous code

--- sec.club/src/components/ProjectWall/ProjectWall.js
+++ sec.club/src/components/ProjectWall/ProjectWall.js
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types'
 import "./ProjectWall.scss"
 import { NavLink } from "react-router-dom"
 import { motion, useViewportScroll, useSpring } from 'framer-motion'
-import { Drawer } from "@material-ui/core"
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
-import Button from "../Button/Button.js";
 import PROJECTS from "../../views/SLP/Projects"
 
 class Project extends React.Component {
@@ -49,18 +45,6 @@ const ProjectWall = () => {
     showNames: false,
   })
 
-  const toggleDrawer = (side, open) => event => {
-    //Extra key navigation functionality
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return
-    }
-
-    setState({ ...state, [side]: open })
-  }
-
   const handleOnMouseEnter = () => {
     if (state.showNames) { //only set the state if it needs to be reset
       return;
@@ -98,20 +82,6 @@ const ProjectWall = () => {
       {PROJECTS.map((project, index) => (
         <Project project={project} key={`project-${index}`} showName={state.showNames}/>
       ))}
-      {/* <Button kind="icon" className="mobile" onClick={toggleDrawer("top", true)}>
-        <MenuIcon />
-      </Button>
-      {PROJECTS.map((project, index) => (
-        <Project project={project} key={`project-${index}`} />
-      ))}
-      <Drawer anchor="top" open={state.top} onClose={toggleDrawer("top", false)}>
-        <Button kind="icon" onClick={toggleDrawer("top", false)} style={{ width: "24px", margin: "4px" }}>
-          <CloseIcon />
-        </Button>
-        {PROJECTS.map((project, index) => (
-          <Project project={project} className="drawer-item" key={`project-${index}`} onClick={toggleDrawer("top", false)} />
-        ))}
-      </Drawer> */}
     </motion.div>
   )
 }
