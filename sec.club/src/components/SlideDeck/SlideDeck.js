9f35677b2c3301cb971d49197a3b98c04a00b198
David Carpenter <carpenterd777@gmail.com>
2020-08-28 18:48:13 -0400

    User can navigate backwards within slides using right click

--- sec.club/src/components/SlideDeck/SlideDeck.js
+++ sec.club/src/components/SlideDeck/SlideDeck.js
@@ -15,34 +15,62 @@ class Slide extends React.Component {
 class SlideDeck extends React.Component {
     constructor(props){
         super(props);
-        this.state = {currentSlide: 0};
+        this.state = {currentSlide: -1};
+
+        //find the first slide child element if elem 0 is not a Slide
+        if(!this.isTypeSlide(this.state.currentSlide)){
+            this.state = {currentSlide: this.findNextSlide()}
+        }
+
         this.handleClick = this.handleClick.bind(this);
+        this.handleOnContext = this.handleOnContext.bind(this);
+    }
+
+    isInbounds = (index) => {
+        return index >= 0 && index <= this.props.children.length - 1;
+    }
+
+    isTypeSlide = (index) => {
+        return this.isInbounds(index) && this.props.children[index].type.name === "Slide";
+    }
+
+    isFirstChild = (index) => {
+        return index === 0;
+    }
+
+    isLastChild = (index) => {
+        return index === this.props.children.length - 1;
+    }
+
+    findNextSlide = () => {
+        let next = this.state.currentSlide + 1;
+
+        while(!this.isTypeSlide(next) && !this.isLastChild(next)){
+            next++;
+        }
+        return next;
     }
 
     nextSlide() {
-        let isInbounds = (index) => {
-            return index > 0 && index <= this.props.children.length - 1;
+        if (this.isLastChild(this.state.currentSlide)){
+            return;
         }
+        let next = this.findNextSlide();
+        this.setState({currentSlide: next})
+    }
 
-        let isTypeSlide = (index) => {
-            return isInbounds(index) && this.props.children[index].type.name === "Slide";
-        }
-
-       let isLastChild = (index) => {
-            return index === this.props.children.length - 1;
-        }
-
-        if (isLastChild(this.state.currentSlide)){
+    prevSlide(){
+        if (this.isFirstChild(this.state.currentSlide)){
             return;
         }
 
-        let next = this.state.currentSlide + 1;
+        let prev = this.state.currentSlide - 1;
 
-        while(!isTypeSlide(next) && !isLastChild(next)){
-            next++;
+        while(!this.isTypeSlide(prev) && !this.isFirstChild(prev)){
+            prev--;
         }
 
-        this.setState({currentSlide: next})
+        this.setState({currentSlide: prev})
     }
 
 
@@ -50,9 +78,14 @@ class SlideDeck extends React.Component {
         this.nextSlide();
     }
 
+    handleOnContext(e){
+        e.preventDefault();
+        this.prevSlide();
+    }
+
     render() {
         return (
-            <div onClick={this.handleClick}>
+            <div onClick={this.handleClick} onContextMenu={this.handleOnContext}>
             {this.props.children[this.state.currentSlide]}
             </div>
         );
--- sec.club/src/views/SlideDeckDemo/SlideDeckDemo.js
+++ sec.club/src/views/SlideDeckDemo/SlideDeckDemo.js
@@ -7,14 +7,16 @@ class SlideDeckDemo extends React.Component {
         return(
         <Container>
             <SlideDeck>
+                <h1>I dont belong at the beginning</h1>
                 <Slide>
                     <h1>Slide 1</h1>
                 </Slide>
                 <Slide>
                     <h1>Slide 2</h1>
                 </Slide>
+                <h1>I'm not a slide??!! I guess I'll be skipped...</h1>
                 <Slide>
-                    <h1>Slide 3</h1>
+                    <h1>Slide 3 (But Child 5!)</h1>
                 </Slide>
             </SlideDeck>
         </Container>
