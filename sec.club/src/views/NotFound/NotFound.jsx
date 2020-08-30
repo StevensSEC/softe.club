e2588fa4902e4543c6a2472aee335997916f53d5
Carson McManus <carson.mcmanus1@gmail.com>
2020-08-29 20:05:50 -0400

    refactor logo to use framer-motion instead, closes #123

--- sec.club/src/views/NotFound/NotFound.jsx
+++ sec.club/src/views/NotFound/NotFound.jsx
@@ -1,11 +1,9 @@
 import React from "react"
 import { Grid, Box } from "@material-ui/core"
 import { useLocation } from "react-router-dom"
-import { ReactSVG } from "react-svg"
-import logo from "../../assets/logo.svg"
-import "./logo-static.scss"
 import DocumentTitle from "../../components/DocumentTitle/DocumentTitle"
 import "./NotFound.scss"
+import Logo from "../../components/Logo/Logo.jsx";
 
 export default function NotFoundView() {
 
@@ -16,14 +14,12 @@ export default function NotFoundView() {
 			<DocumentTitle title="Uh oh! (404 Not Found)"/>
 			<Grid container direction="column" justify="center" alignItems="center" className="grid">
 				<Grid item>
-					<ReactSVG
-						className="logo"
-						src={logo} />
+					<Logo className="logo" />
 				</Grid>
 				<Grid item>
 					<div className="message">There doesn't seem to be a page called &quot;{location}&quot; on our website. Sorry about that!</div>
 				</Grid>
 			</Grid>
 		</Box>
-	); 
+	);
 }
