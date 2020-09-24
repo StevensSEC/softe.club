8b348b0f472d41b141ab078fdaec523e6dc7fd65
“Carson <carson.mcmanus1@gmail.com>
2020-09-24 18:05:03 -0400

    organize articles a bit, add a little more documentation for articles

--- src/Router.js
+++ src/Router.js
@@ -52,7 +52,7 @@ const ROUTES = [
     },
     {
         path: "/git-cheatsheet",
-        articleProps: { source: "git-cheatsheet.md", title: "Git Cheatsheet", },
+        articleProps: { source: "cheatsheets/git.md", title: "Git Cheatsheet", },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
@@ -90,12 +90,12 @@ const ROUTES = [
     },
     {
         path: "/bash-cheatsheet",
-        articleProps: { source: "bash-cheatsheet.md", title: "Bash Cheatsheet" },
+        articleProps: { source: "cheatsheets/bash.md", title: "Bash Cheatsheet" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/powershell-cheatsheet",
-        articleProps: { source: "powershell-cheatsheet.md", title: "Powershell Cheatsheet" },
+        articleProps: { source: "cheatsheets/powershell.md", title: "Powershell Cheatsheet" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
@@ -113,42 +113,42 @@ const ROUTES = [
     },
     {
         path: "/tutorial/install-wsl",
-        articleProps: { source: "events/pimp-my-terminal/howto-install-wsl.md", title: "How to Install the Windows Subsystem for Linux" },
+        articleProps: { source: "tutorials/howto-install-wsl.md", title: "How to Install the Windows Subsystem for Linux" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/enable-colors-mac",
-        articleProps: { source: "events/pimp-my-terminal/howto-enable-terminal-colors-mac.md", title: "How to Enable Terminal Colors on Mac" },
+        articleProps: { source: "tutorials/howto-enable-terminal-colors-mac.md", title: "How to Enable Terminal Colors on Mac" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/iterm2-import-colors",
-        articleProps: { source: "events/pimp-my-terminal/howto-install-iterm2-themes.md", title: "How to Install iTerm2 Color Themes" },
+        articleProps: { source: "tutorials/howto-install-iterm2-themes.md", title: "How to Install iTerm2 Color Themes" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/set-default-shell",
-        articleProps: { source: "events/pimp-my-terminal/howto-set-default-shell.md", title: "How to set the default shell" },
+        articleProps: { source: "tutorials/howto-set-default-shell.md", title: "How to set the default shell" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/ensure-bashrc-profile-setup",
-        articleProps: { source: "events/pimp-my-terminal/howto-ensure-bashrc-profile-setup.md", title: "How to make sure your bashrc/profile is set up" },
+        articleProps: { source: "tutorials/howto-ensure-bashrc-profile-setup.md", title: "How to make sure your bashrc/profile is set up" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/cli-text-editors",
-        articleProps: { source: "events/pimp-my-terminal/howto-use-cli-text-editors.md", title: "How to use terminal text editors" },
+        articleProps: { source: "tutorials/howto-use-cli-text-editors.md", title: "How to use terminal text editors" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/vscode-theme",
-        articleProps: { source: "events/pimp-my-terminal/howto-vscode-set-theme.md", title: "How to set your theme in VS Code" },
+        articleProps: { source: "tutorials/howto-vscode-set-theme.md", title: "How to set your theme in VS Code" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
         path: "/tutorial/set-default-terminal",
-        articleProps: { source: "events/pimp-my-terminal/howto-set-default-terminal.md", title: "How to set your default terminal" },
+        articleProps: { source: "tutorials/howto-set-default-terminal.md", title: "How to set your default terminal" },
         Component: lazy(() => import(/* webpackChunkName: "article" */ "./views/Article/Article.js")),
     },
     {
--- src/articles/docs/articles.md
+++ src/articles/docs/articles.md
@@ -1,4 +1,15 @@
+# Articles
+
+Articles are markdown files containing text content. The purpose of Articles is to make it easier and faster to add new pages to the website.
+
+There are a couple of important things to keep in mind when creating/editing Articles:
+
+- Tutorials and Guides are **not the same thing**. Tutorials are extremely specific, step-by-step instructions to accomplish a specific goal, eg. "Installing WSL". Guides are a more generalized article, with advice, tips, tricks, etc, allowing the reader to pick and choose what they need, eg. "Picking the right language for your project", "Making a good pull request", "How to use git".
+- If you need more complex behavior for an article, you should create a new view for it. 90% of the time, you should be able to do what you need with just an article. If you keep needing the same thing over and over, consider adding it to the `ArticleView`'s feature set.
+
 # How to write new articles for the website
+
+*Tip: Have the source code for this article open for reference.*
 
 1. Add a new markdown file under `src/articles`
 2. Add a new object of the form below to the `ROUTES` array in `Router.js`:
@@ -50,6 +61,7 @@ def example(arg):
 	return "I am a python function", 2
 ```
 
+*Note: This is an SEC website specific feature.*
 ```python,linux
 # This is some python code, intended for linux
 def example(arg):
