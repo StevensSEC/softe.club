9f3982824d2fb3bc268a1e0702aefeb2d3cb43e0
adapap <19696846+adapap@users.noreply.github.com>
2020-10-03 20:35:56 -0400

    Add ArticleList component, show on Home view

---
+++ src/components/ArticleList/ArticleList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { List, ListItem, Link } from '@material-ui/core';
+import ROUTES from '../../Router';
+
+import './ArticleList.scss';
+
+function collateArticles() {
+  // Articles have a title and a source to be used in the Article view
+  let articles = ROUTES.filter((x) => {
+    return x.hasOwnProperty('articleProps') && !!x.articleProps;
+  }).map((x) => (
+    <ListItem>
+      <Link href={x.articleProps.source}>{x.articleProps.title}</Link>
+    </ListItem>
+  ));
+  return articles;
+}
+
+export default class ArticleList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: collateArticles(),
+    };
+  }
+
+  render() {
+    return (
+      <div className="article-list">
+        <h1>Read Our Articles</h1>
+        <List>{this.state.articles}</List>
+      </div>
+    );
+  }
+}
