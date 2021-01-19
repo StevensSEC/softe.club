aaff11fb07400cd91ff36b041d505482c2bd6f9e
David Carpenter <carpenterd777@gmail.com>
2021-01-19 16:11:51 -0500

    Ran prettier on new files

--- src/components/Contributions/Contributions.js
+++ src/components/Contributions/Contributions.js
@@ -12,23 +12,24 @@ export default function Contributions() {
 	let rendered = semesters.map(data => {
 		return (
 			<div key={data.display}>
-				<span>During {data.display}, we had {data.contributors.length} club members make open source contributions.</span>
+				<span>
+					During {data.display}, we had {data.contributors.length} club members make open
+					source contributions.
+				</span>
 				<ul>
-					{data.contributors.map(username =>
+					{data.contributors.map(username => (
 						<li key={username}>
-							<SEC.Link href={`https://github.com/${username}`}>
-								{username}
-							</SEC.Link>
-						</li>)}
+							<SEC.Link href={`https://github.com/${username}`}>{username}</SEC.Link>
+						</li>
+					))}
 				</ul>
 				<span>Combined, they finished {data.referenced_prs.length} pull requests.</span>
 				<ul>
-					{data.referenced_prs.map(link =>
+					{data.referenced_prs.map(link => (
 						<li key={link}>
-							<SEC.Link href={link}>
-								{link}
-							</SEC.Link>
-						</li>)}
+							<SEC.Link href={link}>{link}</SEC.Link>
+						</li>
+					))}
 				</ul>
 			</div>
 		);
@@ -38,7 +39,9 @@ export default function Contributions() {
 		<div>
 			<h1>Open Source Contributions</h1>
 			<div>
-				<SEC.Link href="https://github.com/orgs/StevensSEC/projects/3">Add your contributions to open source here.</SEC.Link>
+				<SEC.Link href="https://github.com/orgs/StevensSEC/projects/3">
+					Add your contributions to open source here.
+				</SEC.Link>
 			</div>
 			<hr />
 			{rendered}
