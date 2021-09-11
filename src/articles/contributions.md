772d412268504314ef4e2e7e28258eff020a2723
David Carpenter <carpenterd777@gmail.com>
2021-09-10 22:33:24 -0400

    add contributions to contributions page

---
+++ src/articles/contributions.md
@@ -0,0 +1,24 @@
+#### 3 club members have made contributions to the Stevens Web Extenstion repo:
+
+-   Adam Papenhausen (@adapap)
+-   David Carpenter (@carpenterd777)
+-   Carson McManus (@dyc3)
+
+#### 10 club members have made contributions to the OpenTogetherTube repo:
+
+-   Carson McManus (@dyc3)
+-   Adam Papenhausen (@adapap)
+-   David Carpenter (@carpenterd777)
+-   Ian B (@ifkb99)
+-   Simon Wagner (@SWSoftware1044)
+-   Zack Schieberl (@zackatoo)
+-   Tyler Seliber (@Tyler-Seliber)
+-   Keye Li (@nebulouspianist)
+-   Kenny Skelton (@KennethSkelton)
+-   Jordan Fernandes (@Jordan883)
+
+#### 3 people have made contributions to the Quick Quack repo:
+
+-   Blake Zimmerman (@blakezimmerman)
+-   Adam Papenhausen (@adapap)
+-   Carson McManus (@dyc3)
--- src/views/Contributions/Contributions.js
+++ src/views/Contributions/Contributions.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import contrib_2020fall from "../../data/contributions/2020fall.json";
 import Contributions from "../../components/Contributions/Contributions";
 import { Container } from "@material-ui/core";
+import SecMarkdown from "../../components/SecMarkdown/SecMarkdown";
+import { fnFetchMarkdown } from "../../modules/fetchMarkdown";
 
 const ContributionsView = () => {
+	const [markdown, setMarkdown] = useState("");
+    const [__previousSource, setPreviousSource] = useState("");
+
+    useEffect(() => {
+        fnFetchMarkdown("contributions.md", __previousSource, setMarkdown, setPreviousSource);
+    }, [__previousSource])
+
 	return (
 		<Container>
-			<h1>Open Source Contributions</h1>
+			<h1>Contributions</h1>
+			<h2>Open Source Contributions</h2>
 			<hr />
 			<Contributions semesters={[contrib_2020fall]} />
+			<h2>SLP Contributions</h2>
+			<hr />
+			<SecMarkdown markdown={markdown}/>
 		</Container>
 	);
 };
