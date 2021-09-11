246bccd1aefb34aeddfef05d859a0dcdd04f7a88
David Carpenter <carpenterd777@gmail.com>
2021-09-10 23:31:49 -0400

    add some responsiveness

--- src/views/SLP/SLPComponent.tsx
+++ src/views/SLP/SLPComponent.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import SECMarkdown from "../../components/SecMarkdown/SecMarkdown";
 import "./SLP.scss";
 import { fnFetchMarkdown } from "../../modules/fetchMarkdown";
+import { useMediaQuery } from "@material-ui/core";
 
 export type Orientation = "left" | "right";
 interface SLPProps {
@@ -15,11 +16,13 @@ const SLP = (props : SLPProps) : JSX.Element => {
     const [markdown, setMarkdown] = useState("");
     const [__previousSource, setPreviousSource] = useState("");
 
+    const onMobile = useMediaQuery("(min-width:600px)");
+
     useEffect(() => {
         fnFetchMarkdown(props.textSource, __previousSource, setMarkdown, setPreviousSource);
     }, [props.textSource, __previousSource])
 
-    let imageDiv = (
+    let imageDiv = onMobile ? (
         <div className="img-div">
             <img
                 loading="lazy"
@@ -27,7 +30,7 @@ const SLP = (props : SLPProps) : JSX.Element => {
                 alt={props.name}
             />
         </div>
-    )
+    ) : null
     let textDiv = (
         <div className="desc">
             <SECMarkdown markdown={markdown}/>
