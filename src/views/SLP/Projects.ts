bce68e29120f957df2634c91bfe801cfa3562e8c
David Carpenter <carpenterd777@gmail.com>
2021-09-10 22:50:17 -0400

    converted projects file to typescript

--- src/views/SLP/Projects.ts
+++ src/views/SLP/Projects.ts
@@ -2,29 +2,26 @@ import ottScreenshot from "../../assets/slp/opentogethertube/ott-screenshot2.png
 import qqScreenshot from "../../assets/slp/quick-quack.png";
 import sweScreenshot from "../../assets/slp/stevens-web-extension/popup.png";
 
-const PROJECTS = [
+interface SLPInfo {
+	img: any,
+	name: string,
+	markdown: string,
+}
+
+const PROJECTS : SLPInfo[] = [
 	{
-		path: "stevens-web-extension",
 		img: sweScreenshot,
 		name: "Stevens Web Extension",
-		semester: "Spring",
-		year: "2020",
 		markdown: "slp/stevens-web-extension.md",
 	},
 	{
-		path: "open-together-tube",
 		img: ottScreenshot,
 		name: "Open Together Tube",
-		semester: "Fall",
-		year: "2019",
 		markdown: "slp/open-together-tube.md",
 	},
 	{
-		path: "quick-quack",
 		img: qqScreenshot,
 		name: "Quick Quack",
-		semester: "Spring",
-		year: "2019",
 		markdown: "slp/quick-quack.md",
 	},
 ];
