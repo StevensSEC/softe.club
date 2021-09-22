1983655ecbfa5a4d24afe3a2fc88c79874e4267e
David Carpenter <carpenterd777@gmail.com>
2021-09-22 18:13:26 -0400

    move check better suited for within component

--- src/modules/fetchMarkdown.ts
+++ src/modules/fetchMarkdown.ts
@@ -3,15 +3,7 @@ interface MarkdownResult {
 	__previousSource: string
 }
 
-const fetchMarkdown = async (source: string, __previousSource: string, oldText: string) : Promise<MarkdownResult> => {
-	if (source === __previousSource) {
-		// don't fetch again if the source hasn't changed
-		return {
-			markdown: oldText,
-			__previousSource: __previousSource
-		};
-	}
-
+const fetchMarkdown = async (source: string) : Promise<MarkdownResult> => {
 	let url = require(`../articles/${source}`);
 
 	const response = await fetch(url)
