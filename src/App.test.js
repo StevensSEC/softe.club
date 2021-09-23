929c6dc90a5f3fec4eca7179608599f4ba52f592
David Carpenter <carpenterd777@gmail.com>
2021-09-22 20:43:49 -0400

    replace all events.js with just events

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import ROUTES from "./Router.js";
-import EVENTS from "./Events.js";
+import EVENTS from "./Events";
 import fs from "fs";
 
 expect.extend({
