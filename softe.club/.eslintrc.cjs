8377205f276caa608b46337a278cf035aba73053
Carson McManus <carson.mcmanus1@gmail.com>
2024-03-11 16:02:57 -0400

    fix eslint

--- softe.club/.eslintrc.cjs
+++ softe.club/.eslintrc.cjs
@@ -1,18 +1,65 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	plugins: ["react", "jsx-a11y"],
+	env: {
+		browser: true,
+	},
+	parser: "@babel/eslint-parser",
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+		linkComponents: [{ name: "Link", linkAttribute: "to" }],
+	},
+	extends: ["plugin:react/recommended"],
+	rules: {
+		"no-console": "error",
+		"no-debugger": "error",
+		"no-var": "error",
+		"eqeqeq": "error",
+		"no-restricted-properties": [
+			"error",
+			{
+				object: "document",
+				property: "title",
+			},
+		],
+		"react/no-unescaped-entities": "warn",
+		"react/prop-types": "off",
+		"react/forbid-elements": [
+			"error",
+			{
+				forbid: [
+					{
+						element: "button",
+						message: "Use our custom SEC.Button component instead.",
+					},
+					{
+						element: "a",
+						message: "Use our custom SEC.Link component instead.",
+					},
+					{
+						element: "Link",
+						message: "Use our custom SEC.Link component instead.",
+					},
+					{
+						element: "NavLink",
+						message: "Use our custom SEC.Link component instead.",
+					},
+					{
+						element: "Box",
+						message: "There is no reason to use material-ui's Box component.",
+					},
+				],
+			},
+		],
+		"react/prefer-stateless-function": "warn",
+	},
+};
